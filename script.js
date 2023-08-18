cb748534933295ce469390b74a5ec20d0167dc01
rifatjahan823 <rifatjahan823@gmail.com>
2023-08-18 13:10:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function show_hide() {
+    var click = document.getElementById("accout");
+    if (click.style.display === "none") {
+       click.style.display = "block";
+    } else {
+       click.style.display = "none";
+    }
+ }
